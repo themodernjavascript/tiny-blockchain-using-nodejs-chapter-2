26030e38c5648a0bdacc22497e28e28c4156ff73
Bunlong <bunlong.van@gmail.com>
2018-08-05 22:29:49 +0700

    ???????

--- index.js
+++ index.js
@@ -63,9 +63,9 @@ class Blockchain{
   }
 }
 
-let savjeeCoin = new Blockchain();
+let myCoin = new Blockchain();
 console.log('Mining block 1...');
-savjeeCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
+myCoin.addBlock(new Block(1, "20/07/2017", { amount: 4 }));
 
 console.log('Mining block 2...');
-savjeeCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
+myCoin.addBlock(new Block(2, "20/07/2017", { amount: 8 }));
